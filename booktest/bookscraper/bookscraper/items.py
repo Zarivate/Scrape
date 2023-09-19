3ccb69f7ad5e661d02fb8b409343fd3d0531a374
Zarivate <Zarivate1@gmail.com>
2023-09-18 23:58:38 -0500

    Testing out pipeline effects

--- booktest/bookscraper/bookscraper/items.py
+++ booktest/bookscraper/bookscraper/items.py
@@ -8,5 +8,27 @@ import scrapy
 
 class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
     pass
+
+# Serializers are used to edit the format of values retrieved from the scraping, this one simply deletes any leading
+# values and replaces them with a $. This is normally done in pipelines however but serializers can be good when not
+# scraping large data and have little to no need for any pipelines.  
+def serialize_price(value):
+    return f'$ {str(value)}'
+
+# All the data will now be stored within a BookItem instead of being yielded within the bookspider itself
+class BookItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    upc = scrapy.Field()
+    product_type = scrapy.Field()
+    price_excl_tax = scrapy.Field()
+    price_incl_tax = scrapy.Field()
+    tax = scrapy.Field()
+    availability = scrapy.Field()
+    num_reviews = scrapy.Field()
+    stars = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()
--- booktest/bookscraper/bookscraper/pipelines.py
+++ booktest/bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,39 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        # Get all the field names from the data
+        field_names = adapter.field_names()
+
+        # Removes any whitespace from the strings, excluding the description
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value.strip()
+
+        # Lowercase all the category and product_type strings
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+
+
+        # Convert the prices to float values
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', "tax"]
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = adapter.replace("$", "")
+            adapter[price_key] = float(value)
+
+        # Store only the availability number, not the whole string
+        avail_string = adapter.get('availability')
+        split_string_array = avail_string.split('(')
+        if len(split_string_array) < 2:
+            adapter['availability'] = 0
+        else:
+            avail_array = split_string_array[1].split(' ')
+            adapter['availability'] = int(avail_array[0])
+
         return item
