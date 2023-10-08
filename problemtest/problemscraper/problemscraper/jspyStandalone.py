69325e409ff3388a6d700e7380ec42aa25786807
Zarivate <Zarivate1@gmail.com>
2023-10-07 23:55:40 -0500

    Fixed minor Selenium bug, simple spelling mistake was issue

--- problemtest/problemscraper/problemscraper/jspyStandalone.py
+++ problemtest/problemscraper/problemscraper/jspyStandalone.py
@@ -1,38 +1,41 @@
-# 
-# import scrapy
-# from scrapy.utils.project import get_project_settings
-# #from problemscraper.items import laptopItem
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.service import Service
+# This is a standalone Python file that does the same as js2spider just without the need for any spiders 
+import scrapy
+#from problemscraper.items import laptopItem
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
+# Not necessarily needed anymore in latest Selenium release but runs much faster
+driver_path = "C:\\Users\\izara\\Desktop\\Projects\\scrape\\chromedriver.exe"
+brave_path = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
 
+service = Service(executable_path=driver_path)
+option = webdriver.ChromeOptions()
+option.binary_location = brave_path
 
-#         # Not necessarily needed anymore in latest Selenium release but runs much faster
-# driver_path = "C:\\Users\\izara\\Desktop\\Projects\\scrape\\chromedriver.exe"
-# brave_path = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+# Open browser in incognito mode
+# option.add_argument("--incognito")
 
-# service = Service(executable_path=driver_path)
-# option = webdriver.ChromeOptions()
-# option.binary_location = brave_path
+# Don't close browser until manually closed or operation finishes
+option.add_experimental_option("detach", True)
+# Makes it so browser isn't visible when running 
+option.add_argument("--headless")
 
-#         # Open browser in incognito mode
-# #option.add_argument("--incognito")
-#         # Don't close browser until manually closed
-# option.add_experimental_option("detach", True)
-#         # Makes it so browser isn't visible when running 
-#         #option.add_argument("--headless")
+# Create new Instance of the browser
+browser = webdriver.Chrome(service=service, options=option)
 
-#         # Create new Instance of the browser
-# browser = webdriver.Chrome(service=service, options=option)
+browser.get("https://www.lazada.com.ph/shop-laptops/")
 
-# browser.get("https://www.lazada.com.ph/shop-laptops/")
+# Get the xpath of the product
+xpath='//*[@data-qa-locator="product-item"]//a[text()]'
 
-# xpath='//*[@data-qa-locator="product-item"]//a[text()]'
+# Find the specific product element using the xPath
+link_elements = browser.find_elements(By.XPATH, xpath)
 
-# link_elements = browser.find_elements(By.XPATH, xpath)
-# for link_el in link_elements:
-#     href = link_el.get_attribute("href")
-#     print(href)
+# Fpr every specific product basically
+for link_el in link_elements:
+    # Grab the href and print it out
+    href = link_el.get_attribute("href")
+    print(href)
         
-# browser.quit()
+browser.quit()
--- problemtest/problemscraper/problemscraper/spiders/js2spider.py
+++ problemtest/problemscraper/problemscraper/spiders/js2spider.py
@@ -1,16 +1,17 @@
 import scrapy
-from scrapy.utils.project import get_project_settings
 from problemscraper.items import laptopItem
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 
+# Spider that runs like a traditional spider, uses Selenium to make a request to a page using the browser to grab data
+# that is rendered using Javascript.
 class js2Spider(scrapy.Spider):
     name = "jsSpider2"
 
-    def start_request(self):
-        settings = get_project_settings()
+    def start_requests(self):
+        
         # Not necessarily needed anymore in latest Selenium release but runs much faster
         driver_path = "C:\\Users\\izara\\Desktop\\Projects\\scrape\\chromedriver.exe"
         brave_path = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
@@ -22,9 +23,9 @@ class js2Spider(scrapy.Spider):
         # Open browser in incognito mode
         # option.add_argument("--incognito")
         # Don't close browser until manually closed
-        #option.add_experimental_option("detach", True)
+        # option.add_experimental_option("detach", True)
         # Makes it so browser isn't visible when running 
-        #option.add_argument("--headless")
+        option.add_argument("--headless")
 
         # Create new Instance of the browser
         browser = webdriver.Chrome(service=service, options=option)
@@ -40,6 +41,7 @@ class js2Spider(scrapy.Spider):
         # Pagination would happen around here
         browser.quit()
 
+# Function that creates a laptopItem using the passed in response
     def parse(self, response):
         laptop = laptopItem()
         laptop["price"] = response.css(".pdp-price_color_orange ::text").get()
