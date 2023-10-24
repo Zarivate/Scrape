20a920e764cf7da6cd2807f9076bacf868bb1f06
Zarivate <Zarivate1@gmail.com>
2023-10-23 23:30:58 -0500

    Fixed pagination, proper Selenium implementation now

--- problemtest/problemscraper/problemscraper/spiders/js3spider.py
+++ problemtest/problemscraper/problemscraper/spiders/js3spider.py
@@ -8,9 +8,12 @@ from selenium.webdriver.common.by import By
 # circumvent having to open up a browser instance to first access the data. Same idea as quotespider essentially, just
 # now uses Selenium to open up a browser instance first and then do the same data collection as in the quotespider.
 class JavascriptQuoteSpider(scrapy.Spider):
-    name = "quotesJSSpider"
+    name = "JSSpiderQuotes"
+    allowed_domains = ["quotes.toscrape.com/js/"]
+    start_urls = ["https://quotes.toscrape.com/js/"]
+
     
-    def start_requests(self):
+    def parse(self, response):
         # Not necessarily needed anymore in latest Selenium release but runs much faster
         driver_path = "C:\\Users\\izara\\Desktop\\Projects\\scrape\\chromedriver.exe"
         brave_path = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
@@ -26,11 +29,11 @@ class JavascriptQuoteSpider(scrapy.Spider):
         option.add_argument("--incognito")
 
         # Don't close browser until manually closed
-        option.add_experimental_option("detach", True)
+        # option.add_experimental_option("detach", True)
 
         # Create new browser isntance
         browser = webdriver.Chrome(service=service, options=option)
-        browser.get("https://quotes.toscrape.com/js/") 
+        browser.get("https://quotes.toscrape.com/js/")
 
         # All the paths for the data needed to fill out the QuoteObject fields
         quote_path = '//div[@class="quote"]'
@@ -38,41 +41,41 @@ class JavascriptQuoteSpider(scrapy.Spider):
         quote_author = './/small[@class="author"]'
         quote_tags = './/a[@class="tag"]'
 
-        # Grab all the quotes on the page
-        quotes = browser.find_elements(By.XPATH, quote_path)
-        
-        # Create a new QuoteObject to hold the quote data from the site
-        quote = QuoteObject()
-
-        # Iterate through every quote
-        for element in quotes:
-            # There can be multiple tags so an array is used to store them all and to append to the "tags" field later
-            tag_list = []
-            quote["text"] = element.find_element(By.XPATH, quote_text).text
-            quote["author"] = element.find_element(By.XPATH, quote_author).text
-            # Iterate through every tag
-            for tag in element.find_elements(By.XPATH, quote_tags):
-                # Add them to the list
-                tag_list.append(tag.text)
-            # Set the field to be equal to the list
-            quote["tags"] = tag_list
-            yield quote
-        
-        # Sees whether a next page exists, if so there is more data to scrape
-        next_page = browser.find_element(By.XPATH, "//*[@class='next']//a[@href]")
-
         # If there is a next page, find the href to go to the next page
-        if next_page:
-            # This is another way of doing it which is having the browser manually click the link
-            # next_link = next_page.find_element(By.XPATH, ".//a[@href]").click()
-            href = next_page.get_attribute("href") 
-            print(href)
-            yield scrapy.Request(href, callback=self.parse)
-        browser.quit()
+        while True:
+            try:
+                # Grab all the quotes on the page
+                quotes = browser.find_elements(By.XPATH, quote_path)
             
-# Function that creates a laptopItem using the passed in response
-    def parse(self, response):
-        quote = QuoteObject()
-        laptop["price"] = response.css(".pdp-price_color_orange ::text").get()
-        laptop["name"] = response.css("h1 ::text").get()
-        yield laptop
+                # Create a new QuoteObject to hold the quote data from the site
+                quote = QuoteObject()
+
+                # Iterate through every quote
+                for element in quotes:
+            
+                    quote["text"] = element.find_element(By.XPATH, quote_text).text
+                    quote["author"] = element.find_element(By.XPATH, quote_author).text
+                    # Since there can be multiple tags, they are all first grabbed and stored first
+                    tags = element.find_elements(By.XPATH, quote_tags)
+                    # Then they are iterated through and joined to fill out the "tags" section of the quote object
+                    quote["tags"] = ",".join([tag.text for tag in tags])
+                    # Below is another way of doing what was done above
+                    # tag_list = []
+                    # for tag in element.find_elements(By.XPATH, quote_tags):
+                    #     # Add them to the list
+                    #     tag_list.append(tag.text)
+                    # Set the field to be equal to the list
+                    # quote["tags"] = tag_list
+                    yield quote
+                
+                # Sees whether a next page exists, if so there is more data to scrape
+                next_page = browser.find_element(By.XPATH, "//*[@class='next']//a[@href]")
+                # Click the link to the next page
+                next_page.click()
+            except:
+                print("No more pages to scrape")
+                break    
+        browser.quit()
+
+        
+# Table problem XPATH *//td[contains(@style, "padding-top")]  
